1946d55a95202efb6bb14032f1e0b0653e834f3c
lamees-hemdan <124389652+lamees-hemdan@users.noreply.github.com>
2023-04-13 17:03:19 +0000

    edited js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Lamees Hemdan
+// Created on: April 2023
 // This file contains the JS functions for index.html
+
+// This function converts from Fahrenheit to Celsius
+
+  function fahrenheitToCelsius () {
+// input
+
+  const fahrenheit = parseFloat(document.getElementById('fahrenheit').value);
+// process
+
+  const celsius = (fahrenheit - 32) * 5 / 9;
+
+// output
+
+  document.getElementById('celsius').innerHTML = 'Temperature in Celsius: ' + celsius;
+}
